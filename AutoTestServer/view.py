ca06b82da9180270f996ab7f3dfcc927efd7ad89
zenghui <784515773@qq.com>
2019-10-27 19:01:45 +0800

    new repo for this project
    django frame.

---
+++ AutoTestServer/view.py
@@ -0,0 +1,11 @@
+﻿from django.http import HttpResponse
+from django.shortcuts import render
+ 
+def hello(request):
+    context          = {}
+    context['hello'] = 'Hello World!'
+    return render(request, 'hello.html', context)
+	
+
+def login(request):
+    return HttpResponse("Login Page !!! ")
