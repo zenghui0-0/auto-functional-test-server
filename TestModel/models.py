1cf17c55d8119161e7bfd40cc13d7b5211619572
zenghui <784515773@qq.com>
2019-10-29 22:27:48 +0800

    add devices&&Tasks page

--- TestModel/models.py
+++ TestModel/models.py
@@ -3,3 +3,15 @@ from django.db import models
  
 class Test(models.Model):
     name = models.CharField(max_length=20)
+
+class myDevice(models.Model):
+    DUT_IP    = models.GenericIPAddressField()
+    HDT_IP    = models.GenericIPAddressField()
+    user_name = models.SlugField()
+    password  = models.SlugField()
+    email     = models.EmailField()
+    def __unicode__(self):
+        return self.HDT_IP
+
+class Task(models.Model):
+    name = models.CharField(max_length=20)
