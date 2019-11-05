28430fd9b788b20d57d2c78797c8cb8fc339eb8e
zenghui <784515773@qq.com>
2019-11-05 22:27:16 +0800

    change site head

--- TestModel/admin.py
+++ TestModel/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from TestModel.models import myDevice, Task
 
 # Register your models here.
+admin.site.site_header = 'WELLCOME'
+admin.site.site_title = 'WELLCOME'
+
 admin.site.register([Task, myDevice])
