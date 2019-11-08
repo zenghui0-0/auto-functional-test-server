6f74660e4a41177eb7ca63d1392a07fc22a06b0a
Zeng <hui2zeng@amd.com>
2019-11-08 16:42:40 +0800

    finish login page

--- TestModel/views.py
+++ TestModel/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
-from . import models
+from . import models, forms
 
 # Create your views here.
 
 
 def index(request):
-    pass
-    return render(request, 'login/index.html')
+    if not request.session.get('is_login', None):
+        message = '您还未登录!'
+        return redirect('/login/', locals())
+    return render(request, 'login/index.html', locals())
 
 
 def login(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        if username.strip() and password:  # 确保用户名和密码都不为空      
-            # 用户名字符合法性验证
-            # 密码长度验证
-            # 更多的其它验证.....
+    if request.session.get('is_login', None):  # 不允许重复登录
+        return redirect('/index/')
+    if request.method == 'POST':
+        login_form = forms.UserForm(request.POST)
+        message = 'please check input content!'
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            password = login_form.cleaned_data.get('password')
             try:
                 user = models.User.objects.get(name=username)
             except:
-                return render(request, 'login/login.html')
+                message = 'username not exist!'
+                return render(request, 'login/login.html', locals())
+
             if user.password == password:
+                request.session['is_login'] = True
+                request.session['user_id'] = user.id
+                request.session['user_name'] = user.name
                 return redirect('/index/')
-            return redirect('/index/')
-    return render(request, 'login/login.html')
+                return redirect('/index/')
+            else:
+                message = 'incorrect password!'
+                return render(request, 'login/login.html', locals())
+        else:
+            return render(request, 'login/login.html', locals())
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', locals())
 
 
 def register(request):
@@ -34,5 +48,12 @@ def register(request):
 
 
 def logout(request):
-    pass
+    if not request.session.get('is_login', None):
+        # 如果本来就未登录，也就没有登出一说
+        return redirect("/login/")
+    request.session.flush()
+    # 或者使用下面的方法
+    # del request.session['is_login']
+    # del request.session['user_id']
+    # del request.session['user_name']
     return redirect("/login/")
